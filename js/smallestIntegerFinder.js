7a3ef757af321802b8b8dc0a5314820c8edfb90d
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-13 21:39:41 +0700

    add more perspective function

--- js/smallestIntegerFinder.js
+++ js/smallestIntegerFinder.js
@@ -1,3 +1,23 @@
 const findSmallestInt = (args) => {
   return Math.min(...args);
 };
+
+const findSmallestInt2 = (args) => {
+  return Math.min.apply(null, args);
+};
+
+/*
+The call() method takes arguments separately.
+The apply() method takes arguments as an array.
+*/
+
+const findSmallestInt3 = (args) => {
+  return args.sort((a, b) => a - b)[0];
+};
+
+const findSmallestInt4 = (args) => {
+  args.sort(function (a, b) {
+    return a - b;
+  });
+  return args[0];
+};
