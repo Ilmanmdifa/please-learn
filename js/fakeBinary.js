c06a8120166b2b511b99c8a2647d737f943a9918
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-20 15:03:46 +0700

    add fake binary challenge

---
+++ js/fakeBinary.js
@@ -0,0 +1,28 @@
+/*
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+function fakeBin(x) {
+  return x
+    .split("")
+    .map((n) => (n < 5 ? 0 : 1))
+    .join("");
+}
+
+function fakeBin2(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (Number(str[i]) >= 5) {
+      newStr += 1;
+    } else {
+      newStr += 0;
+    }
+  }
+  return newStr;
+}
+
+function fakeBin(x) {
+  return x.replace(/[0-4]/g, "0").replace(/[5-9]/g, "1");
+}
