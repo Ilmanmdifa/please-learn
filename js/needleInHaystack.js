977d1e7dd1fd2abc926f4903c744537b17140c96
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-22 18:47:33 +0700

    add a needle in haystack challenge

---
+++ js/needleInHaystack.js
@@ -0,0 +1,34 @@
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+*/
+
+function findNeedle(haystack) {
+  return "found the needle at position" + haystack.indexOf("needle");
+}
+
+const findNeedle2 = (haystack) =>
+  `found the needle at position ${haystack.indexOf("needle")}`;
+
+function findNeedle3(haystack) {
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
+
+function findNeedle4(haystack) {
+  let i = 0;
+  while (i < haystack.length) {
+    if (haystack[i] == "needle") {
+      return "found the needle at position " + i;
+    }
+    i++;
+  }
+}
