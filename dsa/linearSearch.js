3af8d80c9d9bfeadcff784907f0526b7713c1c61
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-10-05 20:25:12 +0700

    add linearSearch

---
+++ dsa/linearSearch.js
@@ -0,0 +1,12 @@
+function linearSearch(arr, target) {
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([1, 2, 3, 8, 100], 3));
+console.log(linearSearch([1, 2, 3, 8, 100], 100));
+console.log(linearSearch([1, 2, 3, 8, 100], 199));
